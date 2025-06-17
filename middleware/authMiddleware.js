b5ebe3d4f91ced53c1187c76cf4b10a2e92dff5f
lavia1 <lauraviide02@gmail.com>
2025-06-17 09:11:54 +0300

    f

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const allowedIPs = process.env.ALLOWED_IPS ? process.env.ALLOWED_IPS.split(',') : [];
+//const allowedIPs = process.env.ALLOWED_IPS ? process.env.ALLOWED_IPS.split(',') : [];
 // Checking allowed IPs
 const verifyAdmin = (req, res, next) => {
     const token = req.headers.authorization?.split(" ")[1];
@@ -17,11 +17,11 @@ const verifyAdmin = (req, res, next) => {
     
 
         // Checking if IP address is allowed
-        const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        console.log("Client IP:", clientIP);
-        if (!allowedIPs.includes(clientIP)) {
-            return res.status(403).json({error: "Access denied from this IP"});
-        }
+        //const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        //console.log("Client IP:", clientIP);
+        //if (!allowedIPs.includes(clientIP)) {
+        //    return res.status(403).json({error: "Access denied from this IP"});
+        //}
 
         next(); //Proceed to admin routes
     } catch (error) {
