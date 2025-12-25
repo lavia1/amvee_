0a72c0f7f6854b81023e1d4f21b116d1981eddc2
lavia1 <lauraviide02@gmail.com>
2025-12-25 16:41:50 +0200

    kuvagalleria

---
+++ frontend/src/pages/Kuvagalleria.js
@@ -0,0 +1,131 @@
+import React from "react";
+import "../styles/Photogallery.css";
+
+export default function PhotoGallery() {
+  return (
+    <div className="photo-gallery-container">
+      <aside className="sidebar">
+        <div className="logo">&#8734;</div>
+      </aside>
+
+      <main className="main">
+        
+
+        <div className="gallery">
+          <figure>
+            <img
+              src="/assets/otusetukuva.jpg"
+              alt="BMW E61 535d"
+            />
+            <figcaption>
+              <h3>BMW E61 535d</h3>
+              
+            </figcaption>
+          </figure>
+
+          <figure>
+            <img
+              src="/assets/fkylki.jpg"
+              alt="BWM 535d F11"
+            />
+            <figcaption>
+              <h3>BMW 535d F11</h3>
+              
+            </figcaption>
+          </figure>
+
+          <figure>
+            <img
+              src="/assets/back_lights.jpg"
+              alt="BMW E61 535d takavalot"
+            />
+           
+          </figure>
+             <figure>
+            <img src="/assets/moottori.jpg" alt="moottori" />
+           
+          </figure>
+           <figure>
+            <img src="/assets/vannekuva.jpg" alt="BMW vanteet" />
+            
+              
+          </figure>
+
+           <figure>
+            <img
+              src="/assets/e46.jpg"
+              alt="BMW E46"
+            />
+            
+          </figure>
+           <figure>
+            <img
+              src="/assets/fsisusta.jpg"
+              alt="BMW sisusta"
+            />
+           
+          </figure>
+           <figure>
+            <img
+              src="/assets/otusvalo.jpg"
+              alt="BMW E61 535d etuvalo"
+            />
+            
+          </figure>
+           <figure>
+            <img
+              src="/assets/wires.jpg"
+              alt="johtosarja"
+            />
+            
+          </figure>
+          <figure>
+            <img
+              src="/assets/mustatakakuva.jpg"
+              alt="BMW takakuva"
+            />
+            
+          </figure>
+          <figure>
+            <img
+              src="/assets/bmwmerkki.jpg"
+              alt="BMW merkki "
+            />
+           
+          </figure>
+          <figure>
+            <img
+              src="/assets/sisusta.jpg"
+              alt="BMW sisusta"
+            />
+            
+          </figure>
+          <figure>
+            <img
+              src="/assets/otuskylki.jpg"
+              alt="BMW E61 535d kylki"
+            />
+            
+          </figure>
+          <figure>
+            <img
+              src="/assets/mustaetu.jpg"
+              alt="BMW keula"
+            />
+          
+          </figure>
+            <figure>
+            <img
+              src="/assets/otusetu.jpg"
+              alt="BMW E61 535d etuosa"
+            />
+          
+          </figure>
+          
+
+          
+        </div>
+      </main>
+    </div>
+  );
+}
--- frontend/src/pages/PhotoGallery.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function PhotoGallery() {
-    return(
-        <div></div>
-    );
-};
